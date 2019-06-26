b9ba85502da1af13bd7ce1992962e5c2cf34b9df
Andrew Zito <zitoa@lafayette.edu>
2019-06-26 12:27:20 -0400

    Alphabetize lang strings

--- lang/en/local_navigation_customizer.php
+++ lang/en/local_navigation_customizer.php
@@ -1,13 +1,13 @@
 <?php
 
 $string['pluginname'] = 'Navigation Customizer';
-$string['settings:flatnav_links:desc'] = 'Custom Flatnav Links';
-$string['settings:flatnav_links:subdesc'] = 'Custom links that will be added to the flatnav';
+$string['settings:custom_icons:default'] = 't/envelope | fa-envelope';
+$string['settings:custom_icons:desc'] = 'Custom Icon Map';
+$string['settings:custom_icons:subdesc'] = 'Map to expose fontawesome icons';
 $string['settings:flatnav_links:default'] = "
 Test | www.example.com | t/calc | participants
 Report | /a/fake/report?courseid={COURSEID} | local_navigation_customizer:t/envelope
 Google | www.google.com
 ";
-$string['settings:custom_icons:desc'] = 'Custom Icon Map';
-$string['settings:custom_icons:subdesc'] = 'Map to expose fontawesome icons';
-$string['settings:custom_icons:default'] = 't/envelope | fa-envelope';
+$string['settings:flatnav_links:desc'] = 'Custom Flatnav Links';
+$string['settings:flatnav_links:subdesc'] = 'Custom links that will be added to the flatnav';
